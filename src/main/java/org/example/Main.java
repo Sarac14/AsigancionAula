3260233847a7e98d27d04b8f8ee0c1851a0c8fd5
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-05-20 23:53:47 -0400

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,27 +6,53 @@ import io.javalin.http.staticfiles.Location;
 
 public class Main {
     public static void main(String[] args) {
+       /* Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/publico");
+        }).start(7000);*/
+
         Javalin app = Javalin.create(config -> {
-            config.staticFiles.add("/publico", Location.CLASSPATH);
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress = false;
+                staticFileConfig.aliasCheck = null;
+            });
+        });
+        app.start(7010);
 
-        }).start(8060);
-
-        app.before("/logeado", ctx -> {
+        app.before("/", ctx -> {
             if (!isUserLoggedIn(ctx)) {
                 ctx.redirect("/login");
             }
         });
 
+        app.get("/", ctx -> {
+            ctx.result("Pagina Inicio");
+        });
+
+        app.post("/login", ctx -> {
+            //String username = ctx.formParam("username");
+            // String password = ctx.formParam("password");
+
+
+            ctx.redirect("/");
+        });
 
         app.get("/logeado", ctx -> {
             ctx.result("Usted ya está logeado :D");
         });
 
+       app.get("/login", ctx -> {
+            ctx.render("/publico/formulario.html");
+        });
+
 
     }
 
-    private static boolean isUserLoggedIn(Context ctxContextContext) {
-
+    private static boolean isUserLoggedIn(Context ctx) {
         return false;
     }
+
 }
