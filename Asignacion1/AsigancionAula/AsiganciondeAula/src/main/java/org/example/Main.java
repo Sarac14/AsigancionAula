7952f92789e0eed64dc7452d6380f2da3ef371fd
Sarac14 <101611058+Sarac14@users.noreply.github.com>
2023-06-02 18:30:08 -0400

    Add files via upload

---
+++ Asignacion1/AsigancionAula/AsiganciondeAula/src/main/java/org/example/Main.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.staticfiles.Location;
+
+public class Main {
+    public static void main(String[] args) {
+       /* Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/publico");
+        }).start(7000);*/
+
+        Javalin app = Javalin.create(config -> {
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress = false;
+                staticFileConfig.aliasCheck = null;
+            });
+
+        }).start(7000);
+
+
+        app.before("/", ctx -> {
+            if (!isUserLoggedIn(ctx)) {
+                ctx.redirect("/login");
+            }
+        });
+
+
+
+        app.get("/", ctx -> {
+            ctx.result("Pagina Inicio");
+        });
+
+        app.post("/login", ctx -> {
+            String username = ctx.formParam("username");
+            String password = ctx.formParam("password");
+
+            if (isValidCredentials(username, password)) {
+                // Establecer una sesión de usuario (por ejemplo, mediante una cookie)
+                ctx.sessionAttribute("username", username);
+                ctx.redirect("/");
+            } else {
+                ctx.redirect("/login");
+            }
+        });
+
+        /*app.get("/logeado", ctx -> {
+            String username = ctx.sessionAttribute("username");
+            if (username != null) {
+                ctx.result("Usted ya está logeado :D");
+            } else {
+                ctx.redirect("/login");
+            }
+        });*/
+
+        app.get("/login", ctx -> {
+            ctx.render("/templates/formulario.html");
+
+        });
+
+    }
+
+    private static boolean isValidCredentials(String username, String password) {
+        return username.equals("admin") && password.equals("12345");
+    }
+
+    private static boolean isUserLoggedIn(Context ctx) {
+        String username = ctx.sessionAttribute("username");
+        return username != null;
+    }
+
+}
